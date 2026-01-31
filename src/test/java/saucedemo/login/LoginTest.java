1bd605561997d2f79304dd1f0c6bbdca316113f9
julioanda <julioandarestu90@gmail.com>
2026-01-31 20:59:26 +0700

    Project CI CD

---
+++ src/test/java/saucedemo/login/LoginTest.java
@@ -0,0 +1,23 @@
+package saucedemo.login;
+
+import core.BaseTest;
+import core.DriverManager;
+import org.example.login.LoginPage;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest{
+
+    //test aja trigger pipeline
+    //tambahin lagi
+    @Test(groups = {"smoke"})
+    public void testLogin() {
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver());
+        loginPage.login(config.getProperty("standardUser"), config.getProperty("password"));
+    }
+
+    public void testFailedLogin(){
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver());
+        loginPage.login(config.getProperty("failedUser"), config.getProperty("password"));
+    }
+
+}
